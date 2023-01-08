9de1b9a7f1d7bae3b52a1150ef541384f0db833a
Nemanja Miljkovic <nmiljkovic89@gmail.com>
2023-01-08 13:06:57 +0100

    finish form

---
+++ src/FormPage.js
@@ -0,0 +1,111 @@
+import { useState } from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import {
+  Box,
+  Stepper,
+  Step,
+  StepLabel,
+  Grid,
+  FormHelperText,
+  Button,
+} from "@mui/material";
+import PersonalInfo from "./components/PersonalInfo";
+import AccountDetails from "./components/AccountDetails";
+import ReviewInfo from "./components/ReviewInfo";
+
+const steps = [" Account Details", "Personal Info", "Review and Submit"];
+const validationSchemas = [
+  Yup.object().shape({
+    email: Yup.string().required("Email is required").email("Invalid email"),
+    password: Yup.string().min(4).required("Password is required"),
+    confirmPassword: Yup.string()
+      .min(4)
+      .oneOf([Yup.ref("password")], "Passwords do not match")
+      .required("Required"),
+  }),
+  Yup.object().shape({
+    firstName: Yup.string().required("First Name is required"),
+    lastName: Yup.string().required("Last Name is required"),
+  }),
+];
+
+const FormPage = () => {
+  const [activeStep, setActiveStep] = useState(0);
+
+  const handleBack = () => {
+    setActiveStep((prevStep) => prevStep - 1);
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+      firstName: "",
+      lastName: "",
+      phone: "",
+      residence: "",
+    },
+    validationSchema: validationSchemas[activeStep],
+    onSubmit: () => {
+      if (activeStep === steps.length - 1) {
+        console.log("last step");
+      } else {
+        setActiveStep((prevStep) => prevStep + 1);
+      }
+    },
+  });
+
+  const formContent = (step) => {
+    switch (step) {
+      case 0:
+        return <AccountDetails formik={formik} />;
+      case 1:
+        return <PersonalInfo formik={formik} />;
+      case 2:
+        return <ReviewInfo formik={formik} />;
+      default:
+        return <div>404: Not Found</div>;
+    }
+  };
+
+  return (
+    <Box
+      sx={{
+        maxWidth: "600px",
+        padding: 2,
+      }}
+    >
+      <Stepper activeStep={activeStep} orientation="horizontal">
+        {steps.map((label, index) => (
+          <Step key={index}>
+            <StepLabel>{label}</StepLabel>
+          </Step>
+        ))}
+      </Stepper>
+      <Grid container>
+        <Grid item xs={12} sx={{ padding: "20px" }}>
+          {formContent(activeStep)}
+        </Grid>
+        {formik.errors.submit && (
+          <Grid item xs={12}>
+            <FormHelperText error>{formik.errors.submit}</FormHelperText>
+          </Grid>
+        )}
+        <Grid item xs={12}>
+          <Button disabled={activeStep === 0} onClick={handleBack}>
+            Back
+          </Button>
+          {activeStep === steps.length - 1 ? (
+            <Button onClick={formik.handleSubmit}>Submit</Button>
+          ) : (
+            <Button onClick={formik.handleSubmit}>Next</Button>
+          )}
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+export default FormPage;
